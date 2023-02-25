d81d99350b489bf6d822cb787f447c7eba70bf1e
fayozbekhamidov <fayozbekhamidov0023@gmail.com>
2023-02-25 10:35:56 +0500

    uxgarish

---
+++ work.js
@@ -0,0 +1,26 @@
+// const  name = prompt(ismingiz);
+
+// console.log  (ismingiz);
+
+// const yosh = prompt('yoshingizni kiriting')
+
+// if(yosh>='18'){
+//     console.log(`sizga prava beriladi`);
+
+  
+// }
+
+//   else{
+//    console.log(yosh);
+// }
+
+const kim = prompt("uzingizni tanish tiring")
+
+if(kim=="talaba"){
+    console.log('yaxshi uqing');
+}
+
+else {
+    console.log("Yaxshi ishlang");
+}
+
